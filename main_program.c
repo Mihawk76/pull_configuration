5a6f2e6d36a19cc1f5dd7db47d3f0ea041c5cc34
Daniel <sdanielchp@gmail.com>
2017-05-19 20:35:01 +0700

    adding the last config

--- main_program.c
+++ main_program.c
@@ -5,7 +5,7 @@
 #include "rest_post_sensor.c"
 #include "json_parse.c"
 #include "mysql_api.c"
-
+const char* io_id_data[10][5];
 typedef struct {
 	int AlarmCode;
 	const char* value1;
@@ -30,18 +30,28 @@ int main()
 	//tabel ac
 	int column_type[] = {0,0,1,1,1,1,0,0,0,1,1,1,1,1,1,1};
 	// 0 = varchar, 1 = int
-	char* column_1[] = {"ir_name", "channel", "set_default", "brand", "id_ir"};
 	// infrared
+	char* column_1[] = {"ir_name", "channel", "set_default", "brand", "id_ir"};
 	int column_type_1[] = {0,0,1,0,1};
-	char* column_2[] = {"temp_name", "th_id", "humidity", "threshold"};
 	//temperature
+	char* column_2[] = {"temp_name", "th_id", "humidity", "threshold"};
 	int column_type_2[] = {0,1,1,1};
-	char* column_3[] = {"id_kwh", "phasa", "channel", "start_operation", "end_operation", "lamp_name", "io_id"};
 	//lamp
+	char* column_3[] = {"id_kwh", "phasa", "channel", "start_operation", "end_operation", "lamp_name", "io_id"};
 	int column_type_3[] = {1,0,1,0,0,0,1};
-	char* column_4[] = {"label", "phase", "channel", "voltMin", "voltMax", "vTimeOut", "currentMin","currentMax","curTimeOut"};
 	//main_power
+	char* column_4[] = {"label", "phase", "channel", "voltMin", "voltMax", "vTimeOut", "currentMin","currentMax","curTimeOut"};
 	int column_type_4[] = {0,0,1,1,1,1,1,1,1};
+	//kwh
+	char* column_5[] = {"kwh_name","kwh_id","brand"};
+	int column_type_5[] = {0,1,0};
+	//door
+	char* column_6[] = {"door_name", "io_id", "max_threshold", "channel", "open_logic", "doorTimeOut"};
+	char* column_6_1[] = {"door_name", "io_id", "max_threshold", "i_channel", "open_logic", "doorTimeOut"};
+	int column_type_6[] = {0,1,1,1,1,1};
+	//io
+	char* column_7[] = {"io_id"};
+	int column_type_7[] = {0};
 	//char* location = "http://10.42.0.1/chart/post2.php";
 	//char* location = "http://52.43.48.93/post.php";
 	//char* location = "http://192.168.88.19:1616/dcms/rest/alfa";
@@ -61,6 +71,8 @@ int main()
 	int i;
 	int32_t t = 0;
  	char *content = NULL;
+	int tes = 0;
+	int n_io_id = 0;
 	//printf("%d\n", sizeof kwh);
   get_id("localhost","root","satunol10","paring","main");
 	for (i=0;i<18;i++)
@@ -92,7 +104,7 @@ int main()
  		printf("%s\n", content);
 		sleep(1);
 	}
-
+	//ac
 	content = get_config(location_config, 1002, "ac", 9, column);
  	printf("%s\n", content);
 	json_parse(content, 9, column);
@@ -101,7 +113,6 @@ int main()
   del_config("localhost","root","satunol10","EMS","ac");
 	for(i=0;i<arraylen;i++)
 	{
-		int tes = 0;
 		for (tes = 0; tes <= 80; tes++)
 		{
 			if (alarm_value[tes][0] == 210)//210 ac temp 
@@ -141,7 +152,7 @@ int main()
 		//printf("column %s, data %s, column_type %d, array len %d, column[arraylen],
   	insert_config("localhost","root","satunol10","EMS","ac", column, config_data[i], column_type, n_array, mysql_id);
 	}
-
+	//infrared
 	n_array = (sizeof (column_1))/(sizeof (column_1[0]));
 	content = get_config(location_config, 1002, "infrared", n_array, column_1);
 	json_parse(content, n_array, column_1);
@@ -153,7 +164,7 @@ int main()
   	insert_config("localhost","root","satunol10","EMS","infrared", column_1, config_data[i], column_type_1, n_array, mysql_id);
 	}
 
-
+	//temperature
 	content = get_config(location_config, 1002, "temperature", 4, column_2);
 	json_parse(content, 4, column_2);
  	printf("%s\n", content);
@@ -165,7 +176,7 @@ int main()
   	insert_config("localhost","root","satunol10","EMS","temperature", column_2, config_data[i], column_type_2, n_array, mysql_id);
 	}
 
-
+	//lamp
 	content = get_config(location_config, 1002, "lamp", 7, column_3);
 	json_parse(content, 7, column_3);
  	printf("%s\n", content);
@@ -175,9 +186,12 @@ int main()
 	for(i=0;i<arraylen;i++)
 	{
   	insert_config("localhost","root","satunol10","EMS","lamp", column_3, config_data[i], column_type_3, n_array, mysql_id);
+		io_id_data[n_io_id][0] = config_data[i][6];
+		printf("io id %s\n",io_id_data[n_io_id][0]);
+		n_io_id++;
 	}
 
-
+	//main_power
 	n_array = (sizeof (column_4))/(sizeof (column_4[0]));
 	content = get_config(location_config, 1002, "main_power", n_array, column_4);
  	printf("%s\n", content);
@@ -187,7 +201,67 @@ int main()
 	{
   	insert_config_main_power("localhost","root","satunol10","EMS","main_power", column_4, config_data[i], column_type_4, n_array);
 	}
-  //	
+	//kwh
+	n_array = (sizeof (column_5))/(sizeof (column_5[0]));
+	content = get_config(location_config, 1002, "kwh", n_array, column_5);
+ 	printf("%s\n", content);
+	json_parse(content, n_array, column_5);
+  del_config("localhost","root","satunol10","EMS","kwh");
+	for(i=0;i<arraylen;i++)
+	{
+  	insert_config("localhost","root","satunol10","EMS","kwh", column_5, config_data[i], column_type_5, n_array, mysql_id);
+	}
+	//door
+	n_array = (sizeof (column_6))/(sizeof (column_6[0]));
+	content = get_config(location_config, 1002, "door", n_array-2, column_6);
+ 	printf("%s\n", content);
+	json_parse(content, n_array-2, column_6);
+	printf("%d\n", n_array);
+  del_config("localhost","root","satunol10","EMS","door");
+	for(i=0;i<arraylen;i++)
+	{
+		io_id_data[n_io_id][0] = config_data[i][1];
+		printf("io id door %s n %d\n",io_id_data[n_io_id][0],n_io_id);
+		n_io_id++;
+		for (tes = 0; tes <= 80; tes++)
+		{
+			if (alarm_value[tes][0] == 400)//400 door  
+			{	
+				int temp = 0;
+				config_data[i][n_array-2] = alarm_config[tes].value1;//door timeout
+				config_data[i][n_array-1] = alarm_config[tes].value2;//door open logic (0/1)
+				printf("config temp ketemu\n");
+			}
+				//config_data[i][15] = "100";
+		}
+				//config_data[i][9] = "100";
+				//config_data[i][10] = "100";
+				//config_data[i][11] = "100";			
+				//config_data[i][12] = "100";			
+		//printf("column %s, data %s, column_type %d, array len %d, column[arraylen],
+  	insert_config("localhost","root","satunol10","EMS","door", column_6_1, config_data[i], column_type_6, n_array, mysql_id);
+	}
+	//io
+  del_config("localhost","root","satunol10","EMS","io");
+  for(i=0;i<n_io_id;i++)
+  {
+    for(tes=i+1;tes<n_io_id;tes++)
+    {
+      if(strcmp(io_id_data[i][0],io_id_data[tes][0])==0)
+      {
+        io_id_data[tes][0] = "0";
+      }
+    }
+   }
+  for(i=0;i<n_io_id;i++)
+  {
+		printf("new io id %s\n",io_id_data[i][0]);
+		if(strcmp(io_id_data[i][0],"0")!=0)
+		{
+  		insert_config("localhost","root","satunol10","EMS","io", column_7, io_id_data[i], column_type_7, 1, mysql_id);
+		}
+	}
+ //	
   //trap_th(location, 10, gateway_ID, 1245,1,2,1356,3005,5,6,7);
 	//res_kwh_2 (location, kwh, 4004, 2000, 1003, 4004, 2000, 14, cc1120_TH_ID, channel);*/
 	/*res_kwh_array(location, kwh, kwh, kwh, 
